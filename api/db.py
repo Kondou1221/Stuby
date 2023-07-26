1440e078a8ea27e2dc95cf13433b5ed5fc8b56b6
Yusuke <124753816+Kondou1221@users.noreply.github.com>
2023-07-26 23:04:58 +0900

    Update db.py

--- api/db.py
+++ api/db.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Mysql_DATABASE_URL = "mysql+pymysql://Apicontroll:stubyEteam@stuby-db.cg4rvknxvwgf.us-east-2.rds.amazonaws.com:3306/STUBY?charset=utf8"
 Mysql_DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/STUBY?charset=utf8"
 
 engine = create_engine(Mysql_DATABASE_URL, echo=True)
